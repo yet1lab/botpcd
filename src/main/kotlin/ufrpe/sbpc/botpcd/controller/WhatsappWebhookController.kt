fb07d7b43998fb81309f4674e3a98f8181a8292e
victor-yghor <victor.yghor@ufrpe.br>
2025-05-01 20:51:40 -0300

    put log to see the webhook

--- src/main/kotlin/ufrpe/sbpc/botpcd/controller/WhatsappWebhookController.kt
+++ src/main/kotlin/ufrpe/sbpc/botpcd/controller/WhatsappWebhookController.kt
@@ -1,16 +1,19 @@
 package ufrpe.sbpc.botpcd.controller
 
+import lombok.extern.java.Log
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.logging.Logger
 
 @RestController
+@Log
 class WhatsappWebhookController {
 	@PostMapping("/whatsapp")
 	fun paginaExemplo(@RequestBody request: String): ResponseEntity<String> {
-		println()
+		Logger.getLogger(WhatsappWebhookController::class.java.name).info("Webhook recebido: $request")
 		return ResponseEntity.ok("Webhook recebido com sucesso: $request")
 	}
 }
