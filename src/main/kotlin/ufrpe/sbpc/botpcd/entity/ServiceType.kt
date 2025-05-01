f6ee1ce4791db8b7296d2671e5a46944ca59e467
victor-yghor <victor.yghor@ufrpe.br>
2025-05-01 07:57:24 -0300

    refactor to insert he monitorAssistanceType

--- src/main/kotlin/ufrpe/sbpc/botpcd/entity/ServiceType.kt
+++ src/main/kotlin/ufrpe/sbpc/botpcd/entity/ServiceType.kt
@@ -3,12 +3,18 @@ package ufrpe.sbpc.botpcd.entity
 /**
  *
  */
-sealed class ServiceType(val monitorAssistanceType: MonitorAssistanceType? = null, val needsCommitteeMember: Boolean = false, val disability: Set<Disability>) {
-    object Libras : ServiceType(monitorAssistanceType = MonitorAssistanceType.LIBRAS_MONITOR, disability = mutableSetOf(Disability.DEAFNESS))
+sealed class ServiceType(val needsCommitteeMember: Boolean = false, val disability: Set<Disability>) {
+    object Libras : ServiceType(disability = mutableSetOf(Disability.DEAFNESS)) {
+        val monitorAssistanceType = MonitorAssistanceType.LIBRAS_MONITOR
+    }
     object LibrasInterpreter: ServiceType(needsCommitteeMember = true, disability = mutableSetOf(Disability.DEAFNESS))
-    object Mobility: ServiceType(monitorAssistanceType = MonitorAssistanceType.MOBILITY_MONITOR, disability = mutableSetOf(Disability.MOBILITY_IMPAIRED, Disability.PHYSICAL_DISABILITY, Disability.BLINDED))
+    object Mobility: ServiceType(disability = mutableSetOf(Disability.MOBILITY_IMPAIRED, Disability.PHYSICAL_DISABILITY, Disability.BLINDED)) {
+        val monitorAssistanceType = MonitorAssistanceType.MOBILITY_MONITOR
+    }
     object AudioDescription: ServiceType(needsCommitteeMember = true, disability = mutableSetOf(Disability.DEAFNESS))
-    object NeurodivergentSupport: ServiceType(monitorAssistanceType = MonitorAssistanceType.NEURODIVERGENT_SUPPORT_MONITOR, disability = mutableSetOf(Disability.NEURODIVERGENT))
+    object NeurodivergentSupport: ServiceType(disability = mutableSetOf(Disability.NEURODIVERGENT)) {
+        val monitorAssistanceType = MonitorAssistanceType.NEURODIVERGENT_SUPPORT_MONITOR
+    }
     object GuideInterpreter: ServiceType(needsCommitteeMember = true, disability = mutableSetOf(Disability.DEAFBLINDNESS))
     object HygieneAndNutrition: ServiceType(needsCommitteeMember = true, disability = mutableSetOf(Disability.MOBILITY_IMPAIRED, Disability.PHYSICAL_DISABILITY))
     object Car : ServiceType(needsCommitteeMember = true, disability = mutableSetOf(Disability.PHYSICAL_DISABILITY, Disability.MOBILITY_IMPAIRED
