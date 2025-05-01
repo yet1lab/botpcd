84a1f4e288ca8b754a0e1d92f8cb4d84972df7eb
victor-yghor <victor.yghor@ufrpe.br>
2025-05-01 17:39:10 -0300

    Enriquecer as entities de ServiceType.kt

--- src/main/kotlin/ufrpe/sbpc/botpcd/entity/ServiceType.kt
+++ src/main/kotlin/ufrpe/sbpc/botpcd/entity/ServiceType.kt
@@ -3,21 +3,21 @@ package ufrpe.sbpc.botpcd.entity
 /**
  *
  */
-sealed class ServiceType(val needsCommitteeMember: Boolean = false, val disability: Set<Disability>) {
-    object Libras : ServiceType(disability = mutableSetOf(Disability.DEAFNESS)) {
+sealed class ServiceType(val provider: Provider, val disability: Set<Disability>) {
+    object Libras : ServiceType(provider = Provider.MONITOR, disability = mutableSetOf(Disability.DEAFNESS)) {
         val monitorAssistanceType = MonitorAssistanceType.LIBRAS_MONITOR
     }
-    object LibrasInterpreter: ServiceType(needsCommitteeMember = true, disability = mutableSetOf(Disability.DEAFNESS))
-    object Mobility: ServiceType(disability = mutableSetOf(Disability.MOBILITY_IMPAIRED, Disability.PHYSICAL_DISABILITY, Disability.BLINDED)) {
+    object LibrasInterpreter: ServiceType(provider = Provider.COMMITTEE_MEMBER, disability = mutableSetOf(Disability.DEAFNESS))
+    object Mobility: ServiceType(provider = Provider.MONITOR, disability = mutableSetOf(Disability.MOBILITY_IMPAIRED, Disability.PHYSICAL_DISABILITY, Disability.BLINDED)) {
         val monitorAssistanceType = MonitorAssistanceType.MOBILITY_MONITOR
     }
-    object AudioDescription: ServiceType(needsCommitteeMember = true, disability = mutableSetOf(Disability.DEAFNESS))
-    object NeurodivergentSupport: ServiceType(disability = mutableSetOf(Disability.NEURODIVERGENT)) {
+    object AudioDescription: ServiceType(provider = Provider.COMMITTEE_MEMBER, disability = mutableSetOf(Disability.DEAFNESS))
+    object NeurodivergentSupport: ServiceType(provider = Provider.MONITOR, disability = mutableSetOf(Disability.NEURODIVERGENT)) {
         val monitorAssistanceType = MonitorAssistanceType.NEURODIVERGENT_SUPPORT_MONITOR
     }
-    object GuideInterpreter: ServiceType(needsCommitteeMember = true, disability = mutableSetOf(Disability.DEAFBLINDNESS))
-    object HygieneAndNutrition: ServiceType(needsCommitteeMember = true, disability = mutableSetOf(Disability.MOBILITY_IMPAIRED, Disability.PHYSICAL_DISABILITY))
-    object Car : ServiceType(needsCommitteeMember = true, disability = mutableSetOf(Disability.PHYSICAL_DISABILITY, Disability.MOBILITY_IMPAIRED
+    object GuideInterpreter: ServiceType(provider = Provider.COMMITTEE_MEMBER, disability = mutableSetOf(Disability.DEAFBLINDNESS))
+    object HygieneAndNutrition: ServiceType(provider = Provider.COMMITTEE_MEMBER, disability = mutableSetOf(Disability.MOBILITY_IMPAIRED, Disability.PHYSICAL_DISABILITY))
+    object Car : ServiceType(provider = Provider.COMMITTEE_MEMBER, disability = mutableSetOf(Disability.PHYSICAL_DISABILITY, Disability.MOBILITY_IMPAIRED
         ));
     fun getServicesByDisability(disability: Disability): List<ServiceType> {
         return ServiceType::class.sealedSubclasses.mapNotNull{ it.objectInstance }.filter{ disability in it.disability }
