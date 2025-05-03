962c58a523e9907634c5bca4710f7e063d3cc4e2
Emmanuel N. C. Brito <code@yet1dev.com>
2025-05-02 21:20:17 -0300

    feat: Complete the entity disability

--- src/main/kotlin/ufrpe/sbpc/botpcd/entity/Disability.kt
+++ src/main/kotlin/ufrpe/sbpc/botpcd/entity/Disability.kt
@@ -1,14 +1,24 @@
 package ufrpe.sbpc.botpcd.entity
 
-/**
- * Represents the type of assistance a monitor can provide.
- *
- */
-enum class Disability {
-    DEAFNESS,
-    BLINDED,
-    NEURODIVERGENT,
-    MOBILITY_IMPAIRED,
-    PHYSICAL_DISABILITY,
-    DEAFBLINDNESS
+//=============================================================
+//  Represents the type of assistance a monitor can provide.
+//=============================================================
+enum class Disability(val textOption: String) {
+	BLINDED("Deficiência visual"),
+	DEAFNESS("Deficiência auditiva/surdez"),
+	DEAFBLINDNESS("Surdocegueira"),
+	AUTIST("Transtorno do Espectro Autista"),
+	PHYSICAL_DISABILITY("Deficiência física"),
+	INTELLECTUAL_DISABILITY("Deficiência intelectual"),
+	MOBILITY_IMPAIRED("Não tenho deficiência, mas tenho mobilidade reduzida"),
+	NOTHING("Não tenho deficiência, nem mobilidade reduzida");
+
+	companion object {
+		fun parse(text: String): Disability? {
+			return Disability.entries.find { it.textOption.equals(text, ignoreCase = true) }
+		}
+		fun textList(): Array<String> {
+			return Disability.entries.map { it.textOption }.toTypedArray()
+		}
+	}
 }
