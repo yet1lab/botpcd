1b0d839b1174e237b583b6f8a3d2deb094d43228
Ronaldo Ribeiro <filhoronaldo.ribeiro@gmail.com>
2025-05-13 16:11:48 -0300

    feat: adicionei início e fim de atendimentos
    Implementa início e fim de atendimentos com atualização automática de status, mas sem nada elaborado, apenas básico para usar o enum de UserStatus.

---
+++ src/main/kotlin/ufrpe/sbpc/botpcd/service/AttendanceService.kt
@@ -0,0 +1,31 @@
+package ufrpe.sbpc.botpcd.service
+
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import ufrpe.sbpc.botpcd.entity.*
+import ufrpe.sbpc.botpcd.repository.AttendanceRepository
+
+@Service
+class AttendanceService(
+    private val attendanceRepository: AttendanceRepository,
+    private val userStatusService: UserStatusService
+) {
+
+    fun searchAttendances(): List<Attendance> {
+        return attendanceRepository.findAll()
+    }
+
+    @Transactional
+    fun beginAttendance(attendance: Attendance) {
+        userStatusService.setMonitorStatus(attendance.monitor, UserStatus.BUSY)
+        attendanceRepository.save(attendance)
+    }
+
+    @Transactional
+    fun endAttendance(attendance: Attendance) {
+        userStatusService.setMonitorStatus(attendance.monitor, UserStatus.AVAILABLE)
+        attendance.endDateTime = java.time.LocalDateTime.now()
+        attendanceRepository.save(attendance)
+    }
+
+}
