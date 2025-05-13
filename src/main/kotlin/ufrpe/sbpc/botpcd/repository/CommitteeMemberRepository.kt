771b219bcf5ccd880f4d75c7d527bf9d35990f13
Ronaldo Ribeiro <filhoronaldo.ribeiro@gmail.com>
2025-05-13 16:10:21 -0300

    feat: adicionei a busca de membros por status
    Permite filtrar membros da comissão disponíveis para atendimentos

--- src/main/kotlin/ufrpe/sbpc/botpcd/repository/CommitteeMemberRepository.kt
+++ src/main/kotlin/ufrpe/sbpc/botpcd/repository/CommitteeMemberRepository.kt
@@ -2,7 +2,9 @@ package ufrpe.sbpc.botpcd.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
 import ufrpe.sbpc.botpcd.entity.CommitteeMember
+import ufrpe.sbpc.botpcd.entity.UserStatus
 
 interface CommitteeMemberRepository: JpaRepository<CommitteeMember, Long> {
     fun findByPhoneNumber(phoneNumber: String): CommitteeMember?
+    fun findByStatus(status: UserStatus): List<CommitteeMember>
 }
