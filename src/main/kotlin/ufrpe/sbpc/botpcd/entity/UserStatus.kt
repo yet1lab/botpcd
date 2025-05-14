a8091891048f4b0070a94267d4b967f5ecb65734
Ronaldo Ribeiro <filhoronaldo.ribeiro@gmail.com>
2025-05-13 23:52:10 -0300

    Ajustei comentários
    Co-Authored-By: Victor Yghor Simões dos Santos <124019555+victoryghor@users.noreply.github.com>

--- src/main/kotlin/ufrpe/sbpc/botpcd/entity/UserStatus.kt
+++ src/main/kotlin/ufrpe/sbpc/botpcd/entity/UserStatus.kt
@@ -1,7 +1,12 @@
 package ufrpe.sbpc.botpcd.entity
 
+/* 
+(DISPONIVEL) ele pode receber um atendimento
+(OCUPADO) ele esta em atendimento, mas quando finalizar o codigo trocara para disponivel
+(INDISPONIVEL) ele foi ao banheiro / esta ocupado. Nao vai receber chamado ate que altere seu status manualmente para available
+*/
 enum class UserStatus {
-    AVAILABLE, /* (DISPONIVEL) ele pode receber um atendimento */
-    BUSY, /* (OCUPADO) ele esta em atendimento, mas quando finalizar o codigo trocara para disponivel */ 
-    UNAVAILABLE /* (INDISPONIVEL) ele foi ao banheiro / esta ocupado. Nao vai receber chamado ate que altere seu status manualmente para available*/
+    AVAILABLE, 
+    BUSY, 
+    UNAVAILABLE 
 }
