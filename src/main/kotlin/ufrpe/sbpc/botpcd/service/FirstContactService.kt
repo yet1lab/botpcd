aaf50bf54b40f42e37aaad1dc7bf8e22de28bc20
Ronaldo Ribeiro <filhoronaldo.ribeiro@gmail.com>
2025-05-16 21:50:02 -0300

    Update FirstContactService.kt
    Co-Authored-By: victor-yghor <170154820+victor-yghor@users.noreply.github.com>

--- src/main/kotlin/ufrpe/sbpc/botpcd/service/FirstContactService.kt
+++ src/main/kotlin/ufrpe/sbpc/botpcd/service/FirstContactService.kt
@@ -12,19 +12,29 @@ class FirstContactService(
     private val pwdRepository: PWDRepository,
     private val monitorRepository: MonitorRepository,
     private val committeeMemberRepository: CommitteeMemberRepository,
-    private val registerService: RegisterService
+    private val registerService: RegisterService,
+    private val attendanceStatusService: AttendantStatusService
 ) {
     fun redirectFluxByUserType(phoneNumber: String, change: Change) {
         when {
+            change.value.messages[0].text.body == "BotPCD" && (monitorRepository.findByPhoneNumber(phoneNumber) != null || (committeeMemberRepository.findByPhoneNumber(
+                phoneNumber
+            ) != null)) -> {
+                val attendant = monitorRepository.findByPhoneNumber(phoneNumber) ?: committeeMemberRepository.findByPhoneNumber(
+                    phoneNumber)!!
+                attendanceStatusService.sendStatusChanger(attendant)
+            }
             pwdRepository.findByPhoneNumber(phoneNumber) != null -> {
 
             }
             monitorRepository.findByPhoneNumber(phoneNumber) != null -> {
 
             }
+
             committeeMemberRepository.findByPhoneNumber(phoneNumber) != null -> {
 
             }
+
             else -> {
                 // Usuario não cadastrado
                 registerService.registerPWD(phoneNumber, change)
