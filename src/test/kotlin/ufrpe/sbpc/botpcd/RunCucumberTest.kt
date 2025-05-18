c21ed581dcf8a3e8c67afde66c27509a689f6259
victor-yghor <victor.yghor@ufrpe.br>
2025-05-18 11:14:11 -0300

    put another annotation to run tests with cucumber

--- src/test/kotlin/ufrpe/sbpc/botpcd/RunCucumberTest.kt
+++ src/test/kotlin/ufrpe/sbpc/botpcd/RunCucumberTest.kt
@@ -3,6 +3,7 @@ package ufrpe.sbpc.botpcd
 @Suite
 @IncludeEngines("cucumber")
 @SelectPackages("ufrpe.sbpc.botpcd")
+@RunWith(Cucumber::class)
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "ufrpe.sbpc.botpcd")
 class RunCucumberTest {
