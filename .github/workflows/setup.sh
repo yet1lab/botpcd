5b71920ebcdac33018ca4bc7d59051533d647ee5
victor-yghor <victor.yghor@ufrpe.br>
2025-05-24 19:45:59 -0300

    update the file to download all dependencies

--- .github/workflows/setup.sh
+++ .github/workflows/setup.sh
@@ -27,14 +27,18 @@ sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin d
 #     Certbot and https
 #==============================================
 sudo apt install certbot python-certbot-nginx
-sudo certbot --nginx
+sudo apt search nginx
+sudo apt install nginx
 
 #==============================================
 #       Automated the setup of the github actions
 #==============================================
-cd actions-runner/
-sudo ./svc.sh install
-sudo ./svc.sh start
+# Create a folder
+mkdir actions-runner && cd actions-runnerCopied!
+curl -o actions-runner-linux-x64-2.324.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.324.0/actions-runner-linux-x64-2.324.0.tar.gz
+echo "e8e24a3477da17040b4d6fa6d34c6ecb9a2879e800aa532518ec21e49e21d7b4  actions-runner-linux-x64-2.324.0.tar.gz" | shasum -a 256 -c
+tar xzf ./actions-runner-linux-x64-2.324.0.tar.gz
+
 
 
 
