e51b5d71ddbc4cc23dcd4a5b9d1595480ea02adb
Ronaldo Ribeiro <filhoronaldo.ribeiro@gmail.com>
2025-06-05 21:02:35 -0300

    Adicionei a funcao de iniciar atendimento

--- src/main/kotlin/ufrpe/sbpc/botpcd/service/AttendanceService.kt
+++ src/main/kotlin/ufrpe/sbpc/botpcd/service/AttendanceService.kt
@@ -1,18 +1,35 @@
 package ufrpe.sbpc.botpcd.service
 
 import org.springframework.stereotype.Service
-import ufrpe.sbpc.botpcd.entity.Disability
-import ufrpe.sbpc.botpcd.entity.ServiceType
+import org.springframework.transaction.annotation.Transactional
+import ufrpe.sbpc.botpcd.entity.*
 import ufrpe.sbpc.botpcd.repository.AttendanceRepository
 
 @Service
 class AttendanceService(
     private val attendanceRepository: AttendanceRepository,
     private val whatsappService: WhatsappService,
+    private val attendantStatusService: AttendantStatusService
 ) {
     fun sendServices(botNumber: String, userNumber: String, disability: Disability) {
         val serviceList = ServiceType.getServicesByDisability(disability)
-        val message = whatsappService.createOptions(serviceList.map { it -> it.description })
+        val serviceDescriptions = serviceList.map { it.description }
+        val message = whatsappService.createOptions(options = serviceDescriptions, header = "Escolha um dos serviços abaixo:")
         whatsappService.sendMessage(botNumber, userNumber, message)
+    }
+
+    @Transactional
+    fun beginAttendance(attendance: Attendance) {
+        when (attendance.attendantType) {
+            Provider.MONITOR -> {
+                val monitor = attendance.attendant as? Monitor
+                monitor?.let { attendantStatusService.setMonitorStatus(it, UserStatus.BUSY) }
+            }
+            Provider.COMMITTEE_MEMBER -> {
+                val committeeMember = attendance.attendant as? CommitteeMember
+                committeeMember?.let { attendantStatusService.setCommitteeMemberStatus(it, UserStatus.BUSY) }
+            }
+        }
+        attendanceRepository.save(attendance)
     }
 }
